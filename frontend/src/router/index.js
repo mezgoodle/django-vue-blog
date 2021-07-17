8358a538b3634bdae94051953d3ab0ae3160bea4
Maxim Zavalniuk <mezidiaofficial@gmail.com>
2021-07-17 13:08:45 +0000

    Set routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Post from '@/components/Post'
+import Author from '@/components/Author'
+import PostsByTag from '@/components/PostsByTag'
+import AllPosts from '@/components/AllPosts'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/author/:username', component: Author },
+  { path: '/post/:slug', component: Post },
+  { path: '/tag/:tag', component: PostsByTag },
+  { path: '/', component: AllPosts },
 ]
 
 const router = new VueRouter({
